ed949bcfc4a9e8b8efddee1acf029e279858783f
MartinG <martinjavierguilhem88@gmail.com>
2020-07-03 13:51:35 +0200

    Rocket_Exercise FASE 2

--- src/rocketTest/Models/Thruster.java
+++ src/rocketTest/Models/Thruster.java
@@ -2,19 +2,31 @@ package rocketTest.Models;
 
 public class Thruster {
 	
+//	atributos
 	private static int CONT=1;
 	private int ThrusterID;
+	private int maxPower;
 		
-	public Thruster() {
-		
+//	constructor
+	public Thruster(int maxPower) {
 		this.ThrusterID= CONT;
 		CONT++;
-		
+		this.maxPower=maxPower;
 	}
 
+	
+//	GETTERS y SETTERS
+	public int getMaxPower() {
+		return maxPower;
+	}
+
+	public void setMaxPower(int maxPower) {
+		this.maxPower = maxPower;
+	}
+	
 	@Override
 	public String toString() {
-		return "Thruster [ID:"+ThrusterID+"]";
+		return "Thruster [ID:"+ThrusterID+", maxPower=" + maxPower+"]";
 	}
 
 }
--- src/rocketTest/main.java
+++ src/rocketTest/main.java
@@ -7,18 +7,18 @@ public class main {
 
 	public static void main(String[] args) {
 
-		Rocket rocket1 = new Rocket("x");
-		rocket1.getThrusterList().add(new Thruster());
-		rocket1.getThrusterList().add(new Thruster());
-		rocket1.getThrusterList().add(new Thruster());
+		Rocket rocket1 = new Rocket("32WESSDS");
+		rocket1.getThrusterList().add(new Thruster(10));
+		rocket1.getThrusterList().add(new Thruster(30));
+		rocket1.getThrusterList().add(new Thruster(80));
 		
 		Rocket rocket2 = new Rocket("LDSFJA32");
-		rocket2.getThrusterList().add(new Thruster());
-		rocket2.getThrusterList().add(new Thruster());
-		rocket2.getThrusterList().add(new Thruster());
-		rocket2.getThrusterList().add(new Thruster());
-		rocket2.getThrusterList().add(new Thruster());
-		rocket2.getThrusterList().add(new Thruster());
+		rocket2.getThrusterList().add(new Thruster(30));
+		rocket2.getThrusterList().add(new Thruster(40));
+		rocket2.getThrusterList().add(new Thruster(50));
+		rocket2.getThrusterList().add(new Thruster(50));
+		rocket2.getThrusterList().add(new Thruster(30));
+		rocket2.getThrusterList().add(new Thruster(10));
 		
 		System.out.println(rocket1);
 		System.out.println(rocket2);
